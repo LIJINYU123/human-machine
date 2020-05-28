8e51856f8c8b49e105e262bae59fb43985e58059
jinyu.li <jinyu.li@ecovacs.com>
2020-05-28 08:40:59 +0800

    修改标注员，质检员和项目管理员个人资料页面，从标准组件改成函数组件

--- src/pages/personCenter/profile/components/InspectorProfile.js
+++ src/pages/personCenter/profile/components/InspectorProfile.js
@@ -1,22 +1,20 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Descriptions } from 'antd';
 
-class InspectorProfile extends Component {
-  render() {
-    const { statistics } = this.props;
+const InspectorProfile = props => {
+  const { statistics } = props;
 
-    return (
-      <Fragment>
-        <Descriptions title="数据统计" column={3} style={{ marginTop: '32px' }} bordered>
-          <Descriptions.Item label="参与项目数">{statistics.involvedProjectNum}</Descriptions.Item>
-          <Descriptions.Item label="质检任务数">{statistics.completeTaskNum}</Descriptions.Item>
-          <Descriptions.Item label="质检题目数">{statistics.reviewQuestionNum}</Descriptions.Item>
-          <Descriptions.Item label="要求质检比例(均)">{`${statistics.requireCheckRate}%`}</Descriptions.Item>
-          <Descriptions.Item label="实际质检比例(均)">{`${statistics.actualCheckRate}%`}</Descriptions.Item>
-        </Descriptions>
-      </Fragment>
-    );
-  }
-}
+  return (
+    <Fragment>
+      <Descriptions title="数据统计" column={3} style={{ marginTop: '32px' }} bordered>
+        <Descriptions.Item label="参与项目数">{statistics.involvedProjectNum}</Descriptions.Item>
+        <Descriptions.Item label="质检任务数">{statistics.completeTaskNum}</Descriptions.Item>
+        <Descriptions.Item label="质检题目数">{statistics.reviewQuestionNum}</Descriptions.Item>
+        <Descriptions.Item label="要求质检比例(均)">{`${statistics.requireCheckRate}%`}</Descriptions.Item>
+        <Descriptions.Item label="实际质检比例(均)">{`${statistics.actualCheckRate}%`}</Descriptions.Item>
+      </Descriptions>
+    </Fragment>
+  );
+};
 
 export default InspectorProfile;
--- src/pages/personCenter/profile/components/ProjectManagerProfile.js
+++ src/pages/personCenter/profile/components/ProjectManagerProfile.js
@@ -1,41 +1,39 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Descriptions, Table } from 'antd';
 
-class ProjectManagerProfile extends Component {
-  render() {
-    const { statistics } = this.props;
+const ProjectManagerProfile = props => {
+  const { statistics } = props;
 
-    const columns = [
-      {
-        title: '延期时长',
-        dataIndex: 'delayTime',
-      },
-      {
-        title: '延期项目数',
-        dataIndex: 'delayProjectNum',
-      },
-      {
-        title: '延期率',
-        dataIndex: 'delayRate',
-        render: val => `${val}%`,
-      },
-    ];
+  const columns = [
+    {
+      title: '延期时长',
+      dataIndex: 'delayTime',
+    },
+    {
+      title: '延期项目数',
+      dataIndex: 'delayProjectNum',
+    },
+    {
+      title: '延期率',
+      dataIndex: 'delayRate',
+      render: val => `${val}%`,
+    },
+  ];
 
-    return (
-      <Fragment>
-        <Descriptions title="数据统计" column={2} style={{ marginTop: '32px' }} bordered>
-          <Descriptions.Item label="参与项目数">{statistics.ownProjectNum}</Descriptions.Item>
-          <Descriptions.Item label="项目延期率">{`${statistics.delayRate}% (${statistics.delayProjectNum}/${statistics.ownProjectNum})`}</Descriptions.Item>
-        </Descriptions>
-        <Table
-          columns={columns}
-          pagination={false}
-          dataSource={statistics.delayInfos}
-          style={{ marginTop: '32px' }}
-        />
-      </Fragment>
-    );
-  }
-}
+  return (
+    <Fragment>
+      <Descriptions title="数据统计" column={2} style={{ marginTop: '32px' }} bordered>
+        <Descriptions.Item label="参与项目数">{statistics.ownProjectNum}</Descriptions.Item>
+        <Descriptions.Item label="项目延期率">{`${statistics.delayRate}% (${statistics.delayProjectNum}/${statistics.ownProjectNum})`}</Descriptions.Item>
+      </Descriptions>
+      <Table
+        columns={columns}
+        pagination={false}
+        dataSource={statistics.delayInfos}
+        style={{ marginTop: '32px' }}
+      />
+    </Fragment>
+  );
+};
 
 export default ProjectManagerProfile;
