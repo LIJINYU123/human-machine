c297d12948528b26a660094ddb564b3c30508fa6
jinyu.li <jinyu.li@ecovacs.com>
2020-06-09 13:24:56 +0800

    修改版本号

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,7 @@
 REPO=docker-reg.ecovacs.com/library
 CONTAINER=human-machine
 DATE=`date +%Y%m%d`
-VERSION='0.3.1'
+VERSION='0.3.2'
 
 GIT_HASH=$(git rev-parse --short HEAD)
 if [ "$GIT_HASH" == "" ]; then
